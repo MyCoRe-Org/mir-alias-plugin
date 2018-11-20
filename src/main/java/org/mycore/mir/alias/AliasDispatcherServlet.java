02384e51d7f431124ac6cf080046a404e2b68bcd
Paul Rochowski <Paul.Rochowski@uni-due.de>
2018-11-20 18:44:50 +0100

    Use AliasDispatcherServlet with MCRContentServlet indeed of servletfilter

--- src/main/java/org/mycore/mir/alias/AliasDispatcherServlet.java
+++ src/main/java/org/mycore/mir/alias/AliasDispatcherServlet.java
@@ -2,7 +2,6 @@ package org.mycore.mir.alias;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,9 +10,9 @@ import java.util.concurrent.TimeUnit;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.TransformerException;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -23,11 +22,17 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
+import org.mycore.common.MCRSessionMgr;
+import org.mycore.common.config.MCRConfiguration;
+import org.mycore.common.content.MCRContent;
+import org.mycore.common.content.MCRPathContent;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.datamodel.niofs.MCRPath;
+import org.mycore.frontend.servlets.MCRContentServlet;
 import org.mycore.solr.MCRSolrClientFactory;
 import org.mycore.solr.MCRSolrUtils;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -37,27 +42,36 @@ import org.mycore.solr.MCRSolrUtils;
  *
  * @author Paul Rochowski
  */
-public class AliasDispatcherServlet extends HttpServlet {
+public class AliasDispatcherServlet extends MCRContentServlet {
 
-	private static final long serialVersionUID = -4222669908309310140L;
-	private static Logger LOGGER = LogManager.getLogger();
+	/**
+	 * 
+	 * default generated serial id
+	 */
+	private static final long serialVersionUID = 1L;
 
+	private static final Logger LOGGER = LogManager.getLogger(AliasDispatcherServlet.class);
 
 	// Solr Fieldnames
 	private static final String OBJECT_ID = "id";
 	private static final String ALIAS = "alias";
+
+	public static final String FILE_PATTERN_LAYOUTSERVICE = "MCR.Alias.Filepattern";
 
 	/**
 	 * 
 	 * Client which communicates with MyCoRe Solr Server
 	 */
 	SolrClient solrClient = MCRSolrClientFactory.getMainSolrClient();
-	
+
+	MCRConfiguration configuration = MCRConfiguration.instance();
+
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public MCRContent getContent(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
 		SolrDocumentList results = new SolrDocumentList();
 		String path = request.getPathInfo();
+
 		boolean isforwarded = false;
 
 		if (path != null) {
@@ -77,19 +91,24 @@ public class AliasDispatcherServlet extends HttpServlet {
 				results = getMCRObjectsFromSolr(path);
 
 				/*
-				 * Use request dispatcher to forward alias URL to equal MyCoRe
-				 * Object URL
+				 * Use request dispatcher to forward alias URL to equal MyCoRe Object URL
 				 */
 				if (!results.isEmpty() && results.get(0) != null) {
 
 					RequestDispatcher dispatcher = request.getServletContext()
 							.getRequestDispatcher("/receive/" + results.get(0).getFieldValue(OBJECT_ID));
 
-					dispatcher.forward(request, response);
+					try {
+						dispatcher.forward(request, response);
+						isforwarded = true;
 
-					LOGGER.debug("Alias was found with Object id: " + results.get(0).getFieldValue(OBJECT_ID));
+						LOGGER.info("Alias was found with Object id: " + results.get(0).getFieldValue(OBJECT_ID));
+						LOGGER.info("Alias will be dispatched to to path /reveive/"
+								+ results.get(0).getFieldValue(OBJECT_ID));
+					} catch (ServletException e) {
 
-					isforwarded = true;
+						LOGGER.error("Error on dispatching object with id: " + results.get(0).getFieldValue(OBJECT_ID));
+					}
 				}
 
 				/*
@@ -109,71 +128,113 @@ public class AliasDispatcherServlet extends HttpServlet {
 					LOGGER.debug("Try to resolve file with filename: " + possibleFilename);
 					LOGGER.debug("Looking for derivates in alias: " + aliasPart);
 
-                    /*
-                     * try to resolve mycore object belongs to filename
-                     */
-                    results = getMCRObjectsFromSolr(parsePath(aliasPart));
+					/*
+					 * try to resolve mycore object belongs to filename
+					 */
+					results = getMCRObjectsFromSolr(parsePath(aliasPart));
 
-                    if (!results.isEmpty()) {
+					if (!results.isEmpty()) {
 
-                        Object documentId = results.get(0).getFieldValue(OBJECT_ID);
+						Object documentId = results.get(0).getFieldValue(OBJECT_ID);
 
-                        if (documentId != null && documentId instanceof String) {
+						if (documentId != null && documentId instanceof String) {
 
-                            /*
-                             * Get doument Id as MCRObjectID
-                             */
-                            MCRObjectID mcrObjectId = MCRObjectID.getInstance((String) documentId);
+							/*
+							 * Get doument Id as MCRObjectID
+							 */
+							MCRObjectID mcrObjectId = MCRObjectID.getInstance((String) documentId);
 
-                            /*
-                             * get derivatives
-                             */
-                            List<MCRObjectID> derivatesForDocument = MCRMetadataManager.getDerivateIds(mcrObjectId, 0, TimeUnit.MILLISECONDS);
+							/*
+							 * get derivatives
+							 */
+							List<MCRObjectID> derivatesForDocument = MCRMetadataManager.getDerivateIds(mcrObjectId, 0,
+									TimeUnit.MILLISECONDS);
 
-                            if (derivatesForDocument != null) {
+							if (derivatesForDocument != null) {
 
-                                for (MCRObjectID mcrDerivateID : derivatesForDocument) {
+								for (MCRObjectID mcrDerivateID : derivatesForDocument) {
 
-                                    LOGGER.debug("Looking in derivate " + mcrDerivateID.toString() + " for filename: " + possibleFilename);
+									if (!isforwarded) {
 
-                                    MCRPath mcrPath = MCRPath.getPath(mcrDerivateID.toString(), possibleFilename);
+										LOGGER.debug("Looking in derivate " + mcrDerivateID.toString()
+												+ " for filename: " + possibleFilename);
 
-                                    try {
-                                        Files.readAttributes(mcrPath, BasicFileAttributes.class);
+										MCRPath mcrPath = MCRPath.getPath(mcrDerivateID.toString(), possibleFilename);
 
-                                        LOGGER.info(possibleFilename + " was found in derivate " + mcrDerivateID.toString());
+										Files.readAttributes(mcrPath, BasicFileAttributes.class);
 
-                                        RequestDispatcher dispatcher = request.getServletContext()
-                                                .getRequestDispatcher("/servlets/MCRFileNodeServlet/" + mcrDerivateID.toString() + "/" + possibleFilename);
+										LOGGER.info(possibleFilename + " was found in derivate "
+												+ mcrDerivateID.toString());
 
-                                        dispatcher.forward(request, response);
-                                        isforwarded = true;
+										/*
+										 * Should the file be transformed via getLayoutService() ?
+										 */
+										final String aliasFilePattern = configuration
+												.getString(FILE_PATTERN_LAYOUTSERVICE, "");
 
-                                    } catch (NoSuchFileException e) {
+										if (!aliasFilePattern.isEmpty() && possibleFilename.matches(aliasFilePattern)) {
 
-                                        // do nothing
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+											MCRContent mcrContent = new MCRPathContent(mcrPath);
+
+											try {
+
+												LOGGER.info("Alias was requested on MCR Session: " + MCRSessionMgr.getCurrentSessionID());
+												
+												LOGGER.info("Alias won't be dispatched!");
+												LOGGER.info("File will be transformed via MCRLayoutService: "
+														+ mcrDerivateID.toString() + "/" + possibleFilename);
+												return getLayoutService().getTransformedContent(request, response,
+														mcrContent);
+
+											} catch (TransformerException | SAXException e) {
+												throw new IOException("could not transform content", e);
+											}
+
+										} else {
+
+											RequestDispatcher dispatcher = request.getServletContext()
+													.getRequestDispatcher("/servlets/MCRFileNodeServlet/"
+															+ mcrDerivateID.toString() + "/" + possibleFilename);
+
+											try {
+												dispatcher.forward(request, response);
+												isforwarded = true;
+
+												LOGGER.info(
+														"Alias will be dispatched to to path /servlets/MCRFileNodeServlet/"
+																+ mcrDerivateID.toString() + "/" + possibleFilename);
+
+											} catch (ServletException e) {
+
+												LOGGER.error("Error on dispatching file to MCRFileNodeServlet: "
+														+ mcrDerivateID.toString() + "/" + possibleFilename);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				
+				LOGGER.info("Alias was requested on MCR Session: " + MCRSessionMgr.getCurrentSessionID());
+			}
+		}
 
 		// Error redirect
 		if (!isforwarded) {
 
 			response.sendError(HttpServletResponse.SC_NOT_FOUND, "Requested Alias was not found: " + path);
 		}
+		
+		return null;
 	}
 
 	/**
 	 * 
 	 * Helper method for resolve an alias in solr
 	 * 
-	 * @param aliasPart
-	 *            alias part
+	 * @param aliasPart alias part
 	 * @return Documentlist associated with given alias from solr
 	 */
 	protected SolrDocumentList getMCRObjectsFromSolr(String aliasPart) {
@@ -204,8 +265,7 @@ public class AliasDispatcherServlet extends HttpServlet {
 	/**
 	 * Parses additional chars to given path
 	 * 
-	 * @param path
-	 *            searchpath
+	 * @param path searchpath
 	 * @return path without additional chars
 	 */
 	private String parsePath(String path) {
@@ -226,8 +286,7 @@ public class AliasDispatcherServlet extends HttpServlet {
 	 * Resolving Solr Documents with given searchStr
 	 * 
 	 *
-	 * @param searchStr
-	 *            search String for solr query
+	 * @param searchStr search String for solr query
 	 * @return if there is an indexing in solr, then get the resolved document
 	 * @throws SolrServerException
 	 * @throws IOException
