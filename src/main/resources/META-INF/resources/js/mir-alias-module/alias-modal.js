b4876ec4b3b2f1561df115d068232585202bea44
Paul Rochowski <Paul.Rochowski@uni-due.de>
2018-11-29 15:59:43 +0100

    Get alias context via rest v2 API

--- src/main/resources/META-INF/resources/js/mir-alias-module/alias-modal.js
+++ src/main/resources/META-INF/resources/js/mir-alias-module/alias-modal.js
@@ -1,48 +1,57 @@
 $(document).ready(function() {
+    console.log('alias-modal.js: look into current mycore object metadata and all parents to get the full url')
 
-	
-	console.log('alias-modal.js: look into current mycore object metadata and all parents to get the full url')
-	
-	var currentMyCoreId = getUrlParameter('id');
-	
-	console.log('alias-modal.js: Current edited mycore object is ' +  currentMyCoreId);
-	getAliasContext(currentMyCoreId);
-	
-	
-	// helper methods
-	function getAliasContext(mycoreid) {
-		
-		var urlMycoreObj = webApplicationBaseURL + "api/v2/objects/" + mycoreid;
-		
-		$.ajax({
-			url : urlMycoreObj,
-	        dataType: "xml",
-			type : "GET",
-			success : function(data) {
-				
-				var myCoreObjServFlagNodes = $('servflags',data);
-				
-				//console.log(data);
-				
-				return myCoreObjServFlagNodes.find('servflag[type="alias"]').text();
-			},
-			
-			error : function(error) {
-				console.log("Failed to get Alias context for " + mycoreid);
-				console.log(error);
-			}
-		});
-	}
-	
-	function getUrlParameter(sParam) {
-		var sPageURL = decodeURIComponent(window.location.search.substring(1)), sURLVariables = sPageURL.split('&'), sParameterName, i;
+    var currentMyCoreId = getUrlParameter('id');
 
-		for (i = 0; i < sURLVariables.length; i++) {
-			sParameterName = sURLVariables[i].split('=');
+    console.log('alias-modal.js: Current edited mycore object is ' + currentMyCoreId);
 
-			if (sParameterName[0] === sParam) {
-				return sParameterName[1] === undefined ? true : sParameterName[1];
-			}
-		}
-	}
+    var mycoreIds = []
+    mycoreIds.push(currentMyCoreId);
+
+    var aliasContext = getAliasContext(mycoreIds);
+
+    // helper methods
+    function getAliasContext(mycoreIds) {
+
+        mycoreIds.forEach(function(mycoreId) {
+            $.ajax({
+                url : webApplicationBaseURL + "api/v2/objects/" + mycoreId,
+                dataType : "xml",
+                type : "GET",
+                success : function(data) {
+
+                    var alias = $(data).find('servflag[type="alias"]').text();
+                    var relatedItems = [];
+
+                    $(data).find('mods\\:relatedItem').each(function() {
+                        relatedItems.push($(this).attr('xlink:href'));
+                    });
+
+                    return {
+                        mycoreId : mycoreId,
+                        alias : alias,
+                        relatedItems : (relatedItems.length > 0 ? getAliasContext(relatedItems) : undefined)
+                    };
+
+                },
+
+                error : function(error) {
+                    console.log("Failed to get Alias context for " + mycoreid);
+                    console.log(error);
+                }
+            });
+        });
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)), sURLVariables = sPageURL.split('&'), sParameterName, i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    }
 });
