cb283f7c3e6f40ad9d513118d0f3ea4a1cec0e4c
Paul Rochowski <Paul.Rochowski@uni-due.de>
2018-12-04 10:12:34 +0100

    # correct path resolving on multiple related items
    # We do not need to build the tree first, this logic resolve us all
    unique tree parts in an array

--- src/main/resources/META-INF/resources/js/mir-alias-module/alias-modal.js
+++ src/main/resources/META-INF/resources/js/mir-alias-module/alias-modal.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-
     /*
      * Use promises for the different alias level requests
      */
@@ -18,40 +16,42 @@ $(document).ready(function () {
         aliasCurrentDocument = $("#mir-aliaspart").val();
 
 
-        let aliasTree = new AliasTree();
-        aliasTree.add(aliasCurrentDocument, 'root');
+        let aliasPaths = [];
+
 
         console.log('alias-modal.js: look into related items dependency to get the full url!')
 
-        getAliasContext(getRelatedItemIds(), aliasCurrentDocument, aliasTree);
+        $.each(getRelatedItemIds(), (index, mycoreId) => {
+            getAliasContext(mycoreId, aliasCurrentDocument, aliasPaths);
+        });
 
         function simpletest() {
-            console.log(aliasTree);
+            console.log(aliasPaths);
         }
 
         setTimeout(simpletest, 5000);
     }
 
     // observe related item
-    $('.mir-related-item-search .form-inline').observe('added', 'span', function(changedItem) {
+    $('.mir-related-item-search .form-inline').observe('added', 'span', function (changedItem) {
 
         console.log('alias-modal.js: There was added a new related item. Refresh the alias tree.');
 
-        /*
-         * Get empty alias tree
-         */
-        let aliasTree = new AliasTree();
+
+        let aliasPaths = [];
 
         /*
          * get current field value from alias
          */
         aliasCurrentDocument = $("#mir-aliaspart").val();
-        aliasTree.add(aliasCurrentDocument, 'root');
 
-        getAliasContext(getRelatedItemIds(), aliasCurrentDocument, aliasTree);
+        $.each(getRelatedItemIds(), (index, mycoreId) => {
+            getAliasContext(mycoreId, aliasCurrentDocument, aliasPaths);
+        });
+
 
         function simpletest() {
-            console.log(aliasTree);
+            console.log(aliasPaths);
         }
 
         setTimeout(simpletest, 5000);
@@ -88,26 +88,30 @@ $(document).ready(function () {
 
     }
 
-    function getAliasContext(mycoreIds, parent, aliasTree) {
+    function getAliasContext(mycoreId, path, aliasPaths) {
 
-        $.each(mycoreIds, (index, mycoreId) => {
+        requestMCRObjectMetadata(mycoreId).then((data) => {
 
-            requestMCRObjectMetadata(mycoreId).then((data) => {
+            var alias = $(data).find('servflag[type="alias"]').text();
+            var relatedItems = [];
 
-                var alias = $(data).find('servflag[type="alias"]').text();
-                var relatedItems = [];
-
-                $(data).find('mods\\:mods > mods\\:relatedItem').each(function () {
-                    relatedItems.push($(this).attr('xlink:href'));
-                });
-
-                console.log('alias-modal.js: Build alias Context for mycore id: ' + mycoreId);
-                console.log('alias-modal.js: {mycoreId : ' + mycoreId + ', alias : ' + alias + ', relatedItems : {' + relatedItems + '}');
-
-                aliasTree.add(alias, parent);
-
-                getAliasContext(relatedItems, alias, aliasTree);
+            $(data).find('mods\\:mods > mods\\:relatedItem').each(function () {
+                relatedItems.push($(this).attr('xlink:href'));
             });
+
+            console.log('alias-modal.js: Build alias Context for mycore id: ' + mycoreId);
+            console.log('alias-modal.js: {mycoreId : ' + mycoreId + ', alias : ' + alias + ', relatedItems : {' + relatedItems + '}');
+
+            path = alias + '/' + path;
+
+            if (relatedItems && relatedItems.length) {
+
+                $.each(relatedItems, (index, mycoreId) => {
+                    getAliasContext(mycoreId, path, aliasPaths);
+                });
+            } else {
+                aliasPaths.push(path);
+            }
         });
     }
 
@@ -115,60 +119,4 @@ $(document).ready(function () {
     function isEmpty(value) {
         return typeof value == 'string' && !value.trim() || typeof value == 'undefined' || value === null;
     }
-
 });
-
-class AliasTree {
-    constructor() {
-        this._root = null;
-    }
-
-    _traverse(callback) {
-        function walk(node) {
-            callback(node);
-            node.children.forEach(walk);
-        }
-
-        walk(this._root);
-    }
-
-    add(value, parentValue) {
-        var newNode = {
-            value,
-            children: []
-        };
-
-        if (null === this._root) {
-            this._root = newNode;
-            return;
-        }
-
-        this._traverse(function (node) {
-            if (parentValue === node.value) {
-                node.children.push(newNode);
-            }
-        });
-    }
-
-    remove(value) {
-        this._traverse(function (node) {
-            node.children.some(function (childNode, index) {
-                if (value === childNode.value) {
-                    return !!node.children.splice(index, 1);
-                }
-            });
-        });
-    }
-
-    search(value) {
-        let exists = false;
-
-        this._traverse(function (node) {
-            if (value === node.value) {
-                exists = true;
-            }
-        });
-
-        return exists;
-    }
-}
